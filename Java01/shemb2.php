f2d79ce340f226cf1fe3fc11183f181450b8e8db
Suhejl Vejseli <suhejl.vejseli03@gmail.com>
2023-03-15 23:31:12 +0100

    First Commit

---
+++ Java01/shemb2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+        //Emrat e variablave te specifikuarane kohen e ekzekutimit
+        $workDay1 = "E hene";
+        $workDay2 = "E marte";
+        $workDay3 = "E merkure";
+        $workDay4 = "E enjte";
+        $workDay5 = "E premte";
+
+        for($i = 0;$i<5;$i++){
+            echo "workDay".$i."<br>";
+            
+        }
+
+    ?>
+    
+</body>
+</html>
---
+++ test-branch
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="test1.php" method="post">
+        Emri: <input type="text" name="emri"><br>
+        Mbiemri: <input type="text" name="mbiemri"><br>
+        Adresa: <input type="text" name = "adresa"><br>
+        <input type="submit" >
+    </form>
+</body>
+</html>
